ac75b37dcfc455da90afd63169d1f24d1f15eb75
yongchaekim <37099211+yongchaekim@users.noreply.github.com>
2026-01-22 18:28:53 +0900

    Update Hopscotch.cpp

--- Hopscotch.cpp
+++ Hopscotch.cpp
@@ -24,89 +24,61 @@ void InputData(void) {
     }
 }
 
+int dr[8] = {-1,  1,  0,  0, -1, -1,  1,  1};
+int dc[8] = { 0,  0, -1,  1, -1,  1, -1,  1};
+
 void Touch(int r, int c) {
     int V;
     if (A[r][c] == 0) V = 1;
     else V = 0;
     A[r][c] = V;
 
-    for(int k = 0; k < 4; k++) {
+    for(int k = 0; k < 8; k++) {
         int nr = r;
         int nc = c;
         int flag = 0;
+				int bomb = 0;
+			
 
         for (;;) {
             nr = nr + dr[k];
             nc = nc + dc[k];
             if (nr < 0 || nr >= H || nc < 0 || nc >= W) break;
+						if(A[nr][nc] == 2) {
+							bomb = 1;
+						}
             if (A[nr][nc] == V) {
                 flag = 1;
                 break;
             }
         }
-        if (flag == 1) {
-            nr = r;
+			
+				if (!flag) continue;
+			
+				if(bomb) {
+						nr = r;
             nc = c;
             for (;;) {
                 nr = nr + dr[k];
                 nc = nc + dc[k];
+								if (nr < 0 || nr >= H || nc < 0 || nc >= W) break;
+                A[nr][nc] = V;
+            }
+				} else {
+						nr = r;
+            nc = c;
+            for (;;) {
+                nr += dr[k];
+                nc += dc[k];
                 if (A[nr][nc] == V) {
                     break;
                 }
                 A[nr][nc] = V;
             }
-        }
+				}
+			
     }
 }
-
-/*
-    int dr[8] = {-1,  1,  0,  0, -1, -1,  1,  1};
-int dc[8] = { 0,  0, -1,  1, -1,  1, -1,  1};
-
-void Touch(int r, int c) {
-    int V = (A[r][c] == 0) ? 1 : 0;
-    A[r][c] = V;
-
-    for (int k = 0; k < 8; k++) {
-        int nr = r, nc = c;
-        bool foundV = false;
-        bool bombBetween = false;
-
-        // Scan until we find first V or go out of bounds
-        while (true) {
-            nr += dr[k];
-            nc += dc[k];
-            if (nr < 0 || nr >= H || nc < 0 || nc >= W) break;
-
-            if (A[nr][nc] == 2) bombBetween = true;
-            if (A[nr][nc] == V) { foundV = true; break; }
-        }
-
-        if (!foundV) continue;
-
-        // Apply operation
-        nr = r; nc = c;
-
-        if (!bombBetween) {
-            // Fill only until the first V
-            while (true) {
-                nr += dr[k];
-                nc += dc[k];
-                if (A[nr][nc] == V) break;
-                A[nr][nc] = V;
-            }
-        } else {
-            // Bomb exists: fill ALL cells to the border
-            while (true) {
-                nr += dr[k];
-                nc += dc[k];
-                if (nr < 0 || nr >= H || nc < 0 || nc >= W) break;
-                A[nr][nc] = V; // bombs overwritten too
-            }
-        }
-    }
-}
-*/
 
 int Solve(void) {
     int cnt = 0;
